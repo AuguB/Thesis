3a30dd05e5867a3fc6dfd32445b7cb5e5e19f8f7
augub <augustijndeboer@gmail.com>
2020-06-06 11:39:39 +0200

    Created a folder to store the result for lovelace

--- bake_models.py
+++ bake_models.py
@@ -10,16 +10,28 @@ n_samples = 2048
 
 
 n_samples = 2048
-n_epsilons = [0, 1, 2, 4, 8,16]
-n_gaussian_dims = [2, 4, 8,16]
-gaussian_powers = [1, 2, 3,4]
-gaussian_max_shifts = [0,0.25,0.5,1]
+n_epsilons = [0, 1]
+n_gaussian_dims = [2]
+gaussian_powers = [1]
+gaussian_max_shifts = [0]
 n_layers = 8
-n_epochs = 1024
+n_epochs = 5
 batch_size = 128
 lr = 1e-4
 decay = 0.997
-n_repeats = 5
+n_repeats = 1
+#
+# n_samples = 2048
+# n_epsilons = [0, 1, 2, 4, 8,16]
+# n_gaussian_dims = [2, 4, 8,16]
+# gaussian_powers = [1, 2, 3,4]
+# gaussian_max_shifts = [0,0.25,0.5,1]
+# n_layers = 8
+# n_epochs = 1024
+# batch_size = 128
+# lr = 1e-4
+# decay = 0.997
+# n_repeats = 5
 
 start = time()
 # Create a folder to store the test results
