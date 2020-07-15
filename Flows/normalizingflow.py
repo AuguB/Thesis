a5c572f158fc13c0b2f8d41452ec0b7f683c6a98
augub <augustijndeboer@gmail.com>
2020-07-15 20:15:25 +0200

    Making small changes to run on GPU

--- Flows/normalizingflow.py
+++ Flows/normalizingflow.py
@@ -5,8 +5,9 @@ from torch.distributions import MultivariateNormal
 class normalizingFlow(nn.Module):
     def __init__(self, modulelist, Q):
         super(normalizingFlow, self).__init__()
-        self.locs = nn.Parameter(torch.zeros(Q), requires_grad=False)
-        self.cov = nn.Parameter(torch.diag(torch.ones(Q)),requires_grad=False)
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.locs = torch.zeros(Q).to(device)
+        self.cov = torch.diag(torch.ones(Q)).to(device)
         self.pu = MultivariateNormal(self.locs,self.cov)
         self.parts = nn.ModuleList(modulelist) # a sequence of flows
         self.Q = Q
