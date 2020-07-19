f957b4da2277a772435f292d9127857d934e7620
augub <augustijndeboer@gmail.com>
2020-07-19 16:01:15 +0200

    Increased batch size on MNIST to speed up training.

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ import torch
 if __name__ == "__main__":
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     print(f"Going to train models on {device}")
+    # t = Taster(device, "/home/guus/PycharmProjects/Thesis/Runs/MNIST_2020-7-15_21:10:36")
+    # t.plot_losses()
 
-    for i in ["MNIST", "FMNIST", "KNNIST", "CIFAR10"]:
-        baker = Baker(device, n_layers=4, n_epochs=10, batch_size=256, lr=5e-4, n_repeats=10)
-        folder = baker.bake(i, [0, 1, 2, 4, 8, 16], 0.15, 0.6)
+    for i, v in enumerate(["MNIST", "FMNIST", "KNNIST", "CIFAR10"]):
+        layers = 4 if i < 3 else 6
+        noise = 0.15 if i < 3 else None
+        baker = Baker(device, n_layers=layers, n_epochs=10, batch_size=128, lr=5e-4, n_repeats=10)
+        folder = baker.bake(i, [0, 1, 2, 4, 8, 16], noise, 0.6)
         taster = Taster(device, folder)
         taster.compute_logli(precomputed=False)
         taster.print_best_model_table()
