bdec78f2cf902ca2a0f8e1929945dc66bafd9c93
augub <augustijndeboer@gmail.com>
2020-07-21 17:11:08 +0200

    Improve some minor things

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from Bakery.Taster import Taster
 import torch
 
 if __name__ == "__main__":
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
     print(f"Going to train models on {device}")
     lovelace_runs = "/home/s1003731/thesis/Thesis/Runs"
     folders = ["CIFAR10_2020-7-21_0:10:34", "MNIST_2020-7-19_21:36:47", "KMNIST_2020-7-20_15:53:32",
