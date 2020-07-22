08c951b57ccbc0a5c8d1a760e50446c853398cf4
augub <augustijndeboer@gmail.com>
2020-07-22 14:15:21 +0200

    Improve some minor things

--- Flows/marginalizingflow.py
+++ Flows/marginalizingflow.py
@@ -50,8 +50,9 @@ class marginalizingFlow(nn.Module):
     def logmean(self, log_probs):
         device = log_probs.device
         np_logprobs = log_probs.detach().cpu().numpy().astype(np.float128)
-        log_prob_max = np.max(np_logprobs, axis=0)
-        normalized_log_probs = log_probs - np.repeat(log_prob_max,(1,log_probs.shape[1]))
+        log_prob_max = np.max(np_logprobs, axis=1,keepdims=True)
+        max_array = np.repeat(log_prob_max,log_probs.shape[1], axis = 1)
+        normalized_log_probs = np_logprobs - max_array
         normalized_probs = np.exp(normalized_log_probs)
         mean_of_normalized_probs = np.mean(normalized_probs, axis=1)
         mean_of_normalized_log_probs = np.log(mean_of_normalized_probs)
--- main.py
+++ main.py
@@ -10,6 +10,7 @@ if __name__ == "__main__":
     folders = ["CIFAR10_2020-7-21_0:10:34", "MNIST_2020-7-19_16:2:16", "KMNIST_2020-7-20_15:53:32",
                "FMNIST_2020-7-19_21:36:47"]
 
+
     for i,f in enumerate(folders):
         t = Taster(device, f"{speedy_runs}/{f}")
         t.compute_logli(precomputed=False)
