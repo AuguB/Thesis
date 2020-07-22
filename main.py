6ed8a00ba8f5ba7ce4032f5676ae3764de4b57ae
augub <augustijndeboer@gmail.com>
2020-07-22 13:16:24 +0200

    Improve some minor things

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     print(f"Going to train models on {device}")
     # lovelace_runs = "/home/s1003731/thesis/Thesis/Runs"
-    speedy_runs = "/home/guus/Thesis"
+    speedy_runs = "/home/guus/Thesis/Runs"
     folders = ["CIFAR10_2020-7-21_0:10:34", "MNIST_2020-7-19_21:36:47", "KMNIST_2020-7-20_15:53:32",
                "MNIST_2020-7-19_16:2:16"]
 
